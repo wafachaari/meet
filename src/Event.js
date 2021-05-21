721a42063d7ed7906f0ae8aef71c10f310534fd8
wafachaari <wafachaari30@gmail.com>
2021-05-21 14:31:11 +0200

    updated

--- src/Event.js
+++ src/Event.js
@@ -14,27 +14,32 @@ class Event extends Component {
 
   render() {
     const event = this.props.event;
+    //const dateTime = (event.start.dateTime).toDateString();
+    const eventISODateTime = new Date(event.start.dateTime);
+    const dateTime = eventISODateTime.toDateString();
     return (
       <div className="Event">
- 
         <h2> {event.summary}</h2>
-        <p>{event.location}</p>
+        <p>{dateTime} @TIMEZONE {event.start.timeZone}</p>
+        <p> @ {event.summary} / {event.location}</p>
         <button
           className="show_btn"
           type="text"
           onClick={() => this.handledetails()}
+
         >
-          {!this.state.show ? 'Show Details' : 'Hide Details'}
+
+          {/* /* {!this.state.show ? 'Show Details' : 'Hide Details'}*/}
         </button>
         {
           this.state.show && (
             <div className="show_event">
-              <h2> About event:</h2>
+              <h3> About event:</h3>
               <p className="description">{event.description}</p>
             </div>
           )
         }</div>
-      
+
     );
   }
 }
