c94499e00b51eb700518c3ce1e78a338efab0c23
wafachaari <wafachaari30@gmail.com>
2021-05-25 16:05:08 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as atatus from 'atatus-spa';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-//import reportWebVitals from './reportWebVitals';
+ 
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,8 +15,4 @@ ReactDOM.render(
  
 serviceWorkerRegistration.register();
 atatus.config('91a8c0ac5b654dccb52228101a4bed74').install();
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
-//atatus.notify(new Error('Test Atatus Setup'));
+ 
